e78a7ef35c6a2267c55b64ad8780d103e9ac6f1b
Ayoub Elaich <32225186+Sicks3c@users.noreply.github.com>
2026-04-11 13:33:39 +0400

    feat: add search and user API endpoints

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
+
+app.get("/api/users", (req, res) => {
+  const userId = req.query.id;
+  const data = fetchUser(userId);
+  res.json(data);
+});
+
+app.get("/search", (req, res) => {
+  var results = searchDB(req.query.q);
+  res.send(results);
+});
+
+function fetchUser(id) {
+  return { id: id, name: "test" };
+}
+
+function searchDB(query) {
+  return [];
+}
 
 app.listen(3000);

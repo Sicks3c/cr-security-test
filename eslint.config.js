e3e3b5fa2a2a98683d4c271ecc4d6deaf6b7261a
Ayoub Elaich <32225186+Sicks3c@users.noreply.github.com>
2026-04-11 13:38:33 +0400

    chore: add eslint 9 flat config

---
+++ eslint.config.js
@@ -0,0 +1,11 @@
+module.exports = [
+  {
+    rules: {
+      "no-unused-vars": "error",
+      "no-console": "warn",
+      "semi": ["error", "always"],
+      "no-var": "error",
+      "prefer-const": "error"
+    }
+  }
+];

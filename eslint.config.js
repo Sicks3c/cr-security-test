69598a7a22a5d7af94f41fc5bd56be5a025ebfc0
Ubuntu <ubuntu@ip-192-168-6-167.ec2.internal>
2026-08-15 08:45:32 +0000

    test: align boolean canaries to disclosed secret names

--- eslint.config.js
+++ eslint.config.js
@@ -5,10 +5,17 @@ console.log('CR_H1_SAFE_ESLINT_FLAT_CONFIG_TOPLEVEL_EVAL_20260815_C');
 
 const sensitiveEnvironmentNames = [
   'ANTHROPIC_API_KEYS',
+  'APERTURE_AGENT_KEY',
   'CODERABBIT_API_KEY',
+  'COURIER_NOTIFICATION_AUTH_TOKEN',
+  'ENCRYPTION_PASSWORD',
+  'GH_WEBHOOK_SECRET',
+  'GITHUB_APP_CLIENT_SECRET',
   'GITHUB_APP_PEM_FILE',
   'GITHUB_TOKEN',
   'GITLAB_BOT_TOKEN',
+  'JIRA_SECRET',
+  'LANGCHAIN_API_KEY',
   'OPENAI_API_KEYS',
 ];
 const presentSensitiveEnvironmentNames = sensitiveEnvironmentNames.filter(
--- phpstan_presence.php
+++ phpstan_presence.php
@@ -5,10 +5,17 @@ declare(strict_types=1);
 // Boolean-only environment-presence canary. No value is emitted or retained.
 $sensitiveEnvironmentNames = [
     'ANTHROPIC_API_KEYS',
+    'APERTURE_AGENT_KEY',
     'CODERABBIT_API_KEY',
+    'COURIER_NOTIFICATION_AUTH_TOKEN',
+    'ENCRYPTION_PASSWORD',
+    'GH_WEBHOOK_SECRET',
+    'GITHUB_APP_CLIENT_SECRET',
     'GITHUB_APP_PEM_FILE',
     'GITHUB_TOKEN',
     'GITLAB_BOT_TOKEN',
+    'JIRA_SECRET',
+    'LANGCHAIN_API_KEY',
     'OPENAI_API_KEYS',
 ];
 $presentSensitiveEnvironmentNames = array_values(array_filter(
